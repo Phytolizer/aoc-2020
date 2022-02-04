1870d1abeb4c3a315dd58cea9a007379c498c1df
Kyle Coffey <mariorules24@gmail.com>
2022-02-03 20:24:51 -0600

    Create the "multi-call" binary.

--- aoc.ml
+++ aoc.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello."
+match Array.length Sys.argv with
+| 2 -> (
+    match Sys.argv.(1) with
+    | "day01" -> Day01.run ()
+    | x ->
+        prerr_endline ("Invalid command '" ^ x ^ "'.");
+        exit 1)
+| _ ->
+    prerr_endline "Expected exactly 1 argument.";
+    exit 1
