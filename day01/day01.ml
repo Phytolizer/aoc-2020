ea82ecd230a8eb7385ed73bec48099e18aefd0c2
Kyle Coffey <mariorules24@gmail.com>
2022-02-03 20:52:45 -0600

    Solve day 1.

--- day01/day01.ml
+++ day01/day01.ml
@@ -1,5 +1,19 @@
 open Helpers.File
 
+let solve (input : int list) : int =
+  let rec check_rest xs =
+    let rec check x ys =
+      match ys with
+      | [] -> None
+      | y :: ys -> if x + y = 2020 then Some (x * y) else check x ys
+    in
+    match xs with
+    | [] -> assert false
+    | x :: xs -> (
+        match check x input with None -> check_rest xs | Some value -> value)
+  in
+  check_rest input
+
 let run () : unit =
   let input = List.map int_of_string (read_lines "day01/input.txt") in
-  List.iter (fun line -> Printf.printf "%d\n" line) input
+  print_endline (string_of_int (solve input))
