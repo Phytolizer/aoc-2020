53f45cb3afb177d75f12bbcaeab7bf0af1d4b32a
Kyle Coffey <mariorules24@gmail.com>
2022-02-03 20:42:39 -0600

    Read the input

--- day01/day01.ml
+++ day01/day01.ml
@@ -1,1 +1,5 @@
-let run () = ()
+open Helpers.File
+
+let run () : unit =
+  let input = List.map int_of_string (read_lines "day01/input.txt") in
+  List.iter (fun line -> Printf.printf "%d\n" line) input
