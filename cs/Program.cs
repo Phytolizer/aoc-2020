5d2bc90cf8b86f1a501c0f4578999f26e6bc565c
Kyle Coffey <me@phytolizer.dev>
2022-02-26 21:00:08 -0600

    c# update

--- cs/Program.cs
+++ cs/Program.cs
@@ -1,71 +1,46 @@
-﻿using System.Diagnostics;
-using AdventOfCode;
-using NDesk.Options;
+﻿using AdventOfCode;
 
-void PrintUsage()
+using McMaster.Extensions.CommandLineUtils;
+
+public class Program
 {
-    var mainModule = Process.GetCurrentProcess().MainModule;
-    var programName = "aoc2020";
+    public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
 
-    if (mainModule != null)
+    void RunDay(int day, string inputFile)
     {
-        programName = mainModule.FileName;
+        switch (day)
+        {
+            case 1:
+                Day01.Run(inputFile);
+                break;
+        }
     }
-    Console.WriteLine($"Usage: {programName} [OPTIONS]");
-    Console.WriteLine("  OPTIONS:");
-    Console.WriteLine("    -h,--help            Display this help message");
-    Console.WriteLine("    -d,--day=<DAY>       Set the day to run");
-    Console.WriteLine("    -i,--input=<INPUT>   Set the input file to use");
-}
 
-void RunDay(int day, string inputFile)
-{
-    switch (day)
+    [Option(ShortName = "d")]
+    public int Day { get; } = 0;
+    [Option(ShortName = "i")]
+    public string? Input { get; private set; }
+
+    private void OnExecute()
     {
-        case 1:
-            Day01.Run(inputFile);
-            break;
+        if (Day > 25 || Day < 0)
+        {
+            Console.Error.WriteLine($"{Day} is not a valid day.");
+            Environment.Exit(1);
+        }
+
+        Input ??= $"{Day}/input.txt";
+
+        if (Day == 0)
+        {
+            foreach (var currentDay in Enumerable.Range(1, 25))
+            {
+                RunDay(currentDay, Input);
+            }
+        }
+        else
+        {
+            RunDay(Day, Input);
+        }
     }
-}
-
-var help = false;
-var day = 0;
-var input = "input.txt";
-
-var p = new OptionSet() {
-    { "d|day=", v => day = int.Parse(v) },
-    { "i|input=", v => input = v },
-    { "h|?|help", v => help = v != null },
-};
-
-var extra = p.Parse(args);
-
-if (extra.Count > 0)
-{
-    Console.Error.WriteLine("Unexpected extra arguments.");
-    Environment.Exit(1);
-}
-
-if (help)
-{
-    PrintUsage();
-    Environment.Exit(0);
-}
-if (day > 25 || day < 1)
-{
-    PrintUsage();
-    Console.Error.WriteLine($"{day} is not a valid day.");
-    Environment.Exit(1);
-}
-
-if (day == 0)
-{
-    foreach (var currentDay in Enumerable.Range(1, 25))
-    {
-        RunDay(currentDay, input);
-    }
-}
-else
-{
-    RunDay(day, input);
 }
