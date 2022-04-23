2eab71683f372267fad18d7e61e11477c5909702
Kyle Coffey <me@phytolizer.dev>
2022-04-23 15:18:35 -0500

    Day 2 Part 2 (Haskell)

--- d02-hs/Main.hs
+++ d02-hs/Main.hs
@@ -28,12 +28,18 @@ rowFromString s = Row c (read low, read high) password
     [range, c:_] = splitOn ' ' policy
     [low, high] = splitOn '-' range
 
-isCorrect :: Row -> Bool
-isCorrect (Row c (low, high) password) = low <= n && n <= high
+isCorrectV1 :: Row -> Bool
+isCorrectV1 (Row c (low, high) password) = low <= n && n <= high
   where
     n = length $ filter (== c) password
+
+isCorrectV2 :: Row -> Bool
+isCorrectV2 (Row c (low, high) password) = (a == c) /= (b == c)
+  where
+    a = password !! (low - 1)
+    b = password !! (high - 1)
 
 main :: IO ()
 main =
   interact $
-  (++ "\n") . show . length . filter isCorrect . map rowFromString . lines
+  (++ "\n") . show . length . filter isCorrectV2 . map rowFromString . lines
